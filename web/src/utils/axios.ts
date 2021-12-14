4a013cbc85f25c40087ba12b31596291390d12e9
Simon Piscitelli <simonpiscitelli@gmail.com>
2021-12-14 10:02:31 +0000

    Switch to Vue
    It's sooo much better

--- web/src/utils/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const $axios = axios.create({
-    headers: { "Access-Control-Allow-Origin": "*" },
-});
---
+++ web/src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <hello-world />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "../components/hello-world.vue";
+
+export default defineComponent({
+    name: "Home",
+
+    components: {
+        HelloWorld,
+    },
+});
+</script>
