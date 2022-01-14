e47a605b5f30664943b10d24ed4f20347c2db244
Simon Piscitelli <simonpiscitelli@gmail.com>
2022-01-14 12:31:45 +0000

    Update pathing to relative

--- .ops/heroku-postbuild.sh
+++ .ops/heroku-postbuild.sh
@@ -1,8 +1,9 @@
 #/usr/local/bin bash
 
 GITROOT="$(git rev-parse --show-toplevel)"
+BASEDIR="$(dirname $0)"
 
-SERVICE_DIR="${GITROOT}/${_service}/";
+SERVICE_DIR="${BASEDIR}/../${_service}/";
 
 echo "SERVICE DIR: ${SERVICE_DIR}"
 
